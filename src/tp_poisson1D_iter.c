1a13c80e906095bb12f8d36292b326ca639a733c
Pacome Rousselle <pacomerouselle@gmail.com>
2023-01-05 10:25:47 +0100

    Template
    Template fourni pour commencer l'implémentation des méthodes itératives

--- src/tp_poisson1D_iter.c
+++ src/tp_poisson1D_iter.c
@@ -1,63 +1,110 @@
+/******************************************/
+/* tp2_poisson1D_iter.c                 */
+/* This file contains the main function   */
+/* to solve the Poisson 1D problem        */
+/******************************************/
 #include "lib_poisson1D.h"
+#include "atlas_headers.h"
 
-int main(int argc, char **argv)
+int main(int argc,char *argv[])
+/* ** argc: Number of arguments */
+/* ** argv: Values of arguments */
 {
   int ierr;
   int jj;
   int nbpoints, la;
-  int ku, kl, kv, lab;
+  int ku, kl, lab, kv;
   int *ipiv;
   int info;
   int NRHS;
   double T0, T1;
-  double *MY_RHS, *EX_RHS, *EX_SOL, *X;
-  double **AAB;
+  double *RHS, *SOL, *EX_SOL, *X;
   double *AB;
-
+  double *MB;
+  
   double temp, relres;
 
-  NRHS=1;
-  nbpoints=10;
-  la=nbpoints-2;
-  T0=-5.0;
-  T1=5.0;
-  kv=1;
-  ku=1;
-  kl=1;
-  lab=kv+kl+ku+1;
+  double opt_alpha;
 
-  printf("--------- Poisson 1D (iterative) ---------\n\n");
-  EX_RHS=(double *) malloc(sizeof(double)*la);
+  /* Size of the problem */
+  NRHS=1;
+  nbpoints=12;
+  la=nbpoints-2;
+
+  /* Dirichlet Boundary conditions */
+  T0=5.0;
+  T1=20.0;
+
+  printf("--------- Poisson 1D ---------\n\n");
+  RHS=(double *) malloc(sizeof(double)*la);
+  SOL=(double *) calloc(la, sizeof(double)); 
   EX_SOL=(double *) malloc(sizeof(double)*la);
   X=(double *) malloc(sizeof(double)*la);
-  MY_RHS=(double *) malloc(sizeof(double)*la);
-  AB = (double *) malloc(sizeof(double)*lab*la);
 
-  //Setting functions
+  /* Setup the Poisson 1D problem */
+  /* General Band Storage */
   set_grid_points_1D(X, &la);
-  set_dense_RHS_DBC_1D(EX_RHS,&la,&T0,&T1);
+  set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-  set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-
-  AB = (double *) malloc(sizeof(double)*lab*la);
-
-  set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-  write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
-  write_vec(EX_RHS, &la, "EX_RHS.dat");
+  
+  write_vec(RHS, &la, "RHS.dat");
   write_vec(EX_SOL, &la, "EX_SOL.dat");
   write_vec(X, &la, "X_grid.dat");
 
-  //Richardson
-
-  //Jacobi
-  set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-  //Gauss-Seidel
+  kv=0;
+  ku=1;
+  kl=1;
+  lab=kv+kl+ku+1;
+  
+  AB = (double *) malloc(sizeof(double)*lab*la);
   set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
   
-  free(EX_RHS);
-  free(MY_RHS);
+  /* uncomment the following to check matrix A */
+  // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
+  
+  /********************************************/
+  /* Solution (Richardson with optimal alpha) */
+
+  /* Computation of optimum alpha */
+  opt_alpha = richardson_alpha_opt(&la);
+  printf("Optimal alpha for simple Richardson iteration is : %lf",opt_alpha); 
+
+  /* Solve */
+  double tol=1e-3;
+  int maxit=1000;
+  double *resvec;
+  int nbite=0;
+
+  resvec=(double *) calloc(maxit, sizeof(double));
+
+  /* Solve with Richardson alpha */
+  richardson_alpha(AB, RHS, SOL, &opt_alpha, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+
+  /* Richardson General Tridiag */
+
+  /* get MB (:=M, D for Jacobi, (D-E) for Gauss-seidel) */
+  kv = 1;
+  ku = 1;
+  kl = 1;
+  MB = (double *) malloc(sizeof(double)*(lab)*la);
+  // extract_MB_jacobi_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+  // extract_MB_gauss_seidel_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+  
+  /* Solve with General Richardson */
+  // richardson_MB(AB, RHS, SOL, MB, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+  
+  /* Write solution */
+  write_vec(SOL, &la, "SOL.dat");
+
+  /* Write convergence history */
+  write_vec(resvec, &nbite, "RESVEC.dat");
+
+  free(resvec);
+  free(RHS);
+  free(SOL);
   free(EX_SOL);
   free(X);
   free(AB);
-  printf("\n\n--------- End of iterative methods -----------\n");    
+  free(MB);
+  printf("\n\n--------- End -----------\n");
 }
